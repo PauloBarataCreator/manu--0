00e64c13ff2d9bd417548275c0c132347a85b6e9
Paulo-GITH <87839806+Paulo-GITH@users.noreply.github.com>
2023-04-26 00:07:46 -0300

    Tá avançando a parte do yaml, NLU

--- nlu/test_data.py
+++ nlu/test_data.py
@@ -1,12 +1,35 @@
 import yaml
+import numpy as np
+
 
 data = yaml.safe_load(open('nlu\\train.yml', 'r', encoding='utf-8').read())
 
+inputs, outputs = [],[]
 
 for command in data['commands']:
-  print(command)
+   inputs.append(command['input'].lower())
+   outputs.append('{}\{}'.format(command['entity'],command['action']))
 
+# print(inputs)
+# print(outputs)
+
+# print(command)
 
 # print(data)
-# for command in data['commands']:
-#     print(command)
+
+
+
+
+#Processar textos por meio de: palavras, caracteres, bytes, sub-palavras
+
+chars = set()
+
+for input in inputs + outputs:
+  for ch in input:
+    if ch not in chars:
+      chars.add(ch)
+
+print("Número de caracteres:", len(chars))
+
+
+#Criar dataset
